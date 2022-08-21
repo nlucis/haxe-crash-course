4aa5796e463c624825e3db86026c8a9972527f8e
Arjay Jones <arjay.jones@nlucis.dev>
2022-08-21 13:48:34 -0700

    added conditional code specific to JS

--- bin/main.js
+++ bin/main.js
@@ -3,6 +3,7 @@ var Main = function() { };
 Main.main = function() {
 	console.log("src/Main.hx:3:","Hello, Hax0r!");
 	console.log("src/Main.hx:7:","Goodbye (" + "cruel, cruel" + ") World!");
+	console.log("src/Main.hx:10:","Hello Browser Too!");
 };
 var haxe_iterators_ArrayIterator = function(array) {
 	this.current = 0;
--- src/Main.hx
+++ src/Main.hx
@@ -3,7 +3,11 @@ class Main {
 		trace("Hello, Hax0r!");
 
 		var cruel = "cruel, cruel";
-		
+
 		trace('Goodbye ($cruel) World!');
+
+		#if js
+		trace("Hello Browser Too!");
+		#end
 	}
 }
